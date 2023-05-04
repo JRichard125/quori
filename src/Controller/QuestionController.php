e2c7a50a967114be7f60722429971a615b03e8f1
joakim richard <joakim.richard49@hotmail.com>
2023-05-04 11:31:36 +0200

    feat: possibilte de repondre au question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
 use DateTimeZone;
 use Doctrine\ORM\EntityManagerInterface;
@@ -29,7 +31,7 @@ class QuestionController extends AbstractController
             $em->persist($question);
             $em->flush();
 
-            $this->addFlash('succes', 'Votre question a ete ajoutée');
+            $this->addFlash('success', 'Votre question a ete ajoutée');
 
             return $this->redirectToRoute('home');
         }
@@ -39,10 +41,25 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/question/{id}', name: 'show_question')]
-    public function show(Request $request, Question $question) {
+    public function show(Request $request, Question $question, EntityManagerInterface $em) {
 
-        
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
 
-        return $this->render('question/show.html.twig', ['question' => $question]);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment->setCreatedAt(new \DateTimeImmutable(timezone: new DateTimeZone("Europe/Paris")));
+            $comment->setRating(0);
+            $comment->setQuestion($question);
+
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre réponse a bien ete publiée');
+
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('question/show.html.twig', ['question' => $question, 'form' => $commentForm->createView()]);
     }
 }

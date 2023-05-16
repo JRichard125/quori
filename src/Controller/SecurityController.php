ea1d13e549e4fadd85630626909526a6743a9152
joakim richard <joakim.richard49@hotmail.com>
2023-05-16 12:11:50 +0200

    creation d'un service pour upload l'image de profil

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\ResetPasswordRepository;
 use App\Repository\UserRepository;
+use App\Services\UploadImageService;
 use DateTime;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -36,7 +37,7 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/signup', name: 'signup')]
-    public function signup(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, UserAuthenticatorInterface $userAuthenticator, MailerInterface $mailer): Response
+    public function signup(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, UserAuthenticatorInterface $userAuthenticator, MailerInterface $mailer, UploadImageService $uploaderPicture): Response
     {
         $user = new User();
         $signupForm = $this->createForm(UserType::class, $user);
@@ -49,11 +50,7 @@ class SecurityController extends AbstractController
 
             $picture = $signupForm->get('pictureFile')->getData();
             if($picture) {
-                $folder = $this->getParameter('profile.folder');
-                $ext = $picture->guessExtention() ?? 'bin';
-                $filename = bin2hex(random_bytes(10)) . "." . $ext;
-                $picture->move($folder, $filename);
-                $user->setImage($this->getParameter('profile.folder.public_path') . "/" . $filename); // /profile/dnhsgfsdj.png
+                $user->setImage($uploaderPicture->uploadProfileImage($picture)); // /profile/dnhsgfsdj.png
             } else {
                 $user->setImage("/images/default_profile.png");
             }
